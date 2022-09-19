84c755636199667b13db20017a1df87ef3e0bc3b
riyce <riyce@mail.ru>
2022-09-20 02:49:16 +0300

    add sender

---
+++ workers/sender/base.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class EmailSenderInterface(ABC):
+    @abstractmethod
+    def connect(self) -> None:
+        pass
+
+    @abstractmethod
+    def send_email(
+            self,
+            send_from: str,
+            send_to: str,
+            message: str,
+            template: str
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def disconnect(self) -> None:
+        pass
